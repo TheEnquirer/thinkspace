e077f586a6f64c2470b2bde4f7065d66f0cc4af0
Huxley Marvit <huxmarv@nuevaschool.org>
2021-12-07 07:35:36 +0000

    NW: f*fuck it's node moduels again im going ot bed  now and also the thing is like to old

--- public/main.mjs
+++ public/main.mjs
@@ -1,6 +1,20 @@
 // INITIALIZE
+
 //import * as THREE from 'three';
+//import { Scene, PerspectiveCamera, WebGLRenderer, Mesh, BoxGeometry, MeshBasicMaterial } from 'three';
+//import { Interaction } from 'three.interaction';
+//import { Interaction } from 'three.interaction';
+
+//import * as THREE from 'https://cdn.skypack.dev/three@<version>';
+import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.135.0-pjGUcRG9Xt70OdXl97VF/mode=imports/optimized/three.js';
+//import { Scene, Clock, PerspectiveCamera, WebGLRenderer, Mesh, BoxGeometry, MeshBasicMaterial } from 'https://cdn.skypack.dev/pin/three@v0.135.0-pjGUcRG9Xt70OdXl97VF/mode=imports/optimized/three.js';
 import { FirstPersonControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/FirstPersonControls.js';
+//import { Interaction } from './node_modules/three.interaction';
+import { Interaction } from 'https://cdn.skypack.dev/pin/three.interaction@v0.2.3-OWhEAGFgFHqRauqtJEO2/mode=imports/optimized/three.interaction.js';
+//console.log(Interaction)
+//import three from 'https://cdn.skypack.dev/three';
+//console.log(three)
+
 
 //import { FirstPersonControls } from './jsm/controls/FirstPersonControls.js';
 
@@ -16,14 +30,40 @@ document.body.appendChild( renderer.domElement );
 let controls = new FirstPersonControls( camera, renderer.domElement );
 controls.movementSpeed = 10;
 controls.lookSpeed = 0.05;
+const interaction = new Interaction(renderer, scene, camera);
 //scene.add( player );
 
 // MESHES
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+
+
+
+//const cube = new THREE.Mesh( geometry, material );
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xffffff }),
+);
+
+//cube.cursor = 'pointer';
+//cube.on('click', function(ev) {console.log("clisin")});
 scene.add( cube );
+cube.cursor = 'pointer';
+//cube.on('click', function(ev) {console.log("clicked", ev)});
+console.log(interaction, cube)
+//interaction.onClick()
+//console.log(cube)
+
+//var vector = new THREE.Vector3(
+//     (camera.position.x / window.innerWidth) * 2 - 1, 
+//     - (camera.position.y / window.innerHeight) * 2 + 1, 
+//                0.5 );
+
+//var rayCaster = projector.unprojectVector(vector, camera);
+
+//var intersectedObjects = rayCaster.intersectObjects(objects);
+//console.log(intersectedObjects);
 
 
 camera.position.z = 5;
