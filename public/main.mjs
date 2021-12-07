0e1c7cc4a2270f69739f8ed7c5de53ce84d502ab
Huxley Marvit <huxmarv@nuevaschool.org>
2021-12-07 06:33:13 +0000

    controls!

--- public/main.mjs
+++ public/main.mjs
@@ -1,24 +1,44 @@
-console.log("hewwwwwwww");
+// INITIALIZE
+//import * as THREE from 'three';
+import { FirstPersonControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/FirstPersonControls.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//import { FirstPersonControls } from './jsm/controls/FirstPersonControls.js';
 
-const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene(); // init scene
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); // init camera
+const clock = new THREE.Clock();
+
+const renderer = new THREE.WebGLRenderer(); // init renderer
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+
+let controls = new FirstPersonControls( camera, renderer.domElement );
+controls.movementSpeed = 10;
+controls.lookSpeed = 0.05;
+//scene.add( player );
+
+// MESHES
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+
 camera.position.z = 5;
+
+
+// ANIMATION LOOP
 
 function animate() {
     requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
+    controls.update( clock.getDelta() );
+    //cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 animate();
+
+
+
