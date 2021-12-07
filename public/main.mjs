681e047866a6438da23b25caef32094e3d5f0681
Exr0n <mail@exr0n.com>
2021-12-06 22:00:53 -0800

    format + start.sh

--- public/main.mjs
+++ public/main.mjs
@@ -16,9 +16,9 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
+    requestAnimationFrame( animate );
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+    renderer.render( scene, camera );
 }
 animate();
