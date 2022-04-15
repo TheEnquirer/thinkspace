27d2daa44f80fd129b9b1837bdf64b076c1c193d
Huxley Marvit <huxmarv@nuevaschool.org>
2022-04-15 09:01:36 -0700

    secrets? no. we value honesty here. bEloVeD coMunIty

---
+++ public/.secrets.js
@@ -0,0 +1,5 @@
+const Secrets = {
+    SUPABASE_URL: "https://lfmvhmaqemcopefngcdc.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTQzODA3MiwiZXhwIjoxOTU1MDE0MDcyfQ.ca5PD2k4QFRfO40sIoTHJUAfrIFmNsQwLJYdeVmklgg"
+}
+export default Secrets;
